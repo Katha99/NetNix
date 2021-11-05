1e54bb03401c7e7b04381f6b2325302c3b98b8fa
Katha99 <kathafunny@gmail.com>
2021-11-05 08:22:00 +0100

    Added the overview for the director

---
+++ NetNix/Controllers/DirectorController.cs
@@ -0,0 +1,43 @@
+﻿using NetNix.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NetNix.Controllers
+{
+    public class DirectorController : Controller
+    {
+        public async Task<ActionResult> Index(string id)
+        {
+            DirectorDetailsModel model = await _GetDirectorDetails(id);
+
+            return View("~/Views/Director/Index.cshtml", model);
+        }
+
+        private async Task<DirectorDetailsModel> _GetDirectorDetails(string id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://virtserver.swaggerhub.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("BartvdPost/NetNix/0.2.0/director/" + id);
+
+                DirectorDetailsModel model = new DirectorDetailsModel();
+                if (response.IsSuccessStatusCode)
+                {
+                    model = await response.Content.ReadAsAsync<DirectorDetailsModel>();
+                }
+                else
+                {
+                    //Console.WriteLine("Internal server Error");
+                }
+
+                return model;
+            }
+        }
+    }
+}
