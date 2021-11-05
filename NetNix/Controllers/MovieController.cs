e2118bc21ed2afd972bf1d82761b3b8c8b93b86a
Katha99 <kathafunny@gmail.com>
2021-11-05 08:21:34 +0100

    Added the movie overview with a like button

---
+++ NetNix/Controllers/MovieController.cs
@@ -0,0 +1,90 @@
+﻿using NetNix.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NetNix.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public async Task<ActionResult> Index(string id, string directorId)
+        {
+            MovieDetailsModel model = await _GetMovieDetails(id);
+            model.director.id = directorId;
+
+            return View("~/Views/Movie/Index.cshtml", model);
+        }
+
+        public async Task LikeMovie(string id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://virtserver.swaggerhub.com/");
+                var likeModel = new LikePostModel() { username = "Katharina Rabanus", movieId = id };
+                HttpResponseMessage response = await client.PostAsJsonAsync("BartvdPost/NetNix/0.2.0/like", likeModel);
+            }
+
+            return;
+        }
+
+        private async Task<MovieDetailsModel> _GetMovieDetails(string id)
+        {
+            MovieDetailsModel model = new MovieDetailsModel();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://virtserver.swaggerhub.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("BartvdPost/NetNix/0.2.0/movie/" + id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    model = await response.Content.ReadAsAsync<MovieDetailsModel>();
+
+                    string result = await response.Content.ReadAsStringAsync();
+                    model.image = _GetImageValue(result);
+                }
+
+            }
+
+            return model;
+        }
+        private string _GetImageValue(string result)
+        {
+            int indexImageStart = result.IndexOf("image");
+
+            int indexImageEnd = _GetNextIndexQuotationMark(result, indexImageStart);
+            int indexImageValueStart = _GetNextIndexQuotationMark(result, indexImageEnd);
+            int indexImageValueEnd = _GetNextIndexQuotationMark(result, indexImageValueStart) - 1;
+
+            result = result.Remove(indexImageValueEnd);
+            result = result.Remove(0, indexImageValueStart);
+
+            return result;
+        }
+
+        private int _GetNextIndexQuotationMark(string result, int indexStart)
+        {
+            char quotationMarks = '"';
+            bool quotationMarkFound = false;
+            int index = indexStart;
+
+            while (!quotationMarkFound && index < result.Length)
+            {
+                if (result[index] == quotationMarks)
+                    quotationMarkFound = true;
+                index++;
+            }
+
+            return index;
+        }
+    }
+}
---
+++ NetNix/Models/MovieDetailsModel.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetNix.Models
+{
+    public class MovieDetailsModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public DateTime releaseDate { get; set; }
+        public string shortDescription { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public Director director { get; set; }
+
+        public class Director
+        {
+            public string name { get; set; }
+            public string dateOfBirth { get; set; }
+            public string id { get; set; }
+        }
+    }
+}
