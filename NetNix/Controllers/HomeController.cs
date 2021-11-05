49348c52322660026444eafd9f72d138826c3e59
Katha99 <kathafunny@gmail.com>
2021-11-05 08:19:19 +0100

    Added the home page with the overview for the 5 soon to be released movies

--- NetNix/Controllers/HomeController.cs
+++ NetNix/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using NetNix.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +12,38 @@ namespace NetNix.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            List<MovieOverviewModel> data = await _GetTop5SoonToBeReleasedMoviesAsync();
+
+            HomeViewModel model = new HomeViewModel();
+            model.movieList = data.OrderBy(x => x.releaseDate).Take(5).ToList();
+
+            return View("~/Views/Home/Index.cshtml", model);
         }
 
-        public ActionResult About()
+        private async Task<List<MovieOverviewModel>> _GetTop5SoonToBeReleasedMoviesAsync()
         {
-            ViewBag.Message = "Your application description page.";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://virtserver.swaggerhub.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            return View();
-        }
+                HttpResponseMessage response = await client.GetAsync("BartvdPost/NetNix/0.2.0/soon/");
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                List<MovieOverviewModel> list = new List<MovieOverviewModel>();
+                if (response.IsSuccessStatusCode)
+                {
+                    list = await response.Content.ReadAsAsync<List<MovieOverviewModel>>();
+                }
+                else
+                {
+                    //Console.WriteLine("Internal server Error");
+                }
 
-            return View();
+                return list;
+            }
         }
     }
 }
---
+++ NetNix/Models/MovieOverviewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetNix.Models
+{
+    public class MovieOverviewModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public DateTime releaseDate { get; set; }
+        public directorModel director { get; set; }
+
+        public class directorModel
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+        }
+    }
+}
